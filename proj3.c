5c582c43fbe75663fba8aefa300c5c8bb7a048bd
Alexander Laughlin <44688156+AlexanderLaughlin@users.noreply.github.com>
2020-04-01 11:28:41 -0400

    Add files via upload
    Threads now read lines from file

--- proj3.c
+++ proj3.c
@@ -23,31 +23,38 @@ void main(int argc, char *argv[]){
 
 	FILE *f;
 	char str[25];
+	int numThreads = atoi(argv[1]);	     //threads= number of threads
+	printf("Number of threads to create = %d\n", numThreads);
 
 
 	f = fopen("shakespeare.txt", "r");
 	if (f == NULL){
 		printf("Error opening file\n");
 	}
-
-	if (fgets (str, 25, f)!=NULL) {
-		printf("String = %s\n", str);
+	for (int i = 0; i<numThreads; i++){
+		//read lines from shakespeare.txt and store in array
+		//threads will remove elements from array to add to final string
+	
+	
+		if (fgets (str, 25, f)!=NULL) {
+			printf("String = %s\n", str);
+		}
 	}
 
-	int numThreads = atoi(argv[1]);	     //threads= number of threads
-	printf("Number of threads to create = %d\n", numThreads);	
+		
 	strLen = 10;  
 	pthread_t thread_id[numThreads+1];      //extra space for null character
 
 	assert(pthread_mutex_lock(&lock)==0); //LOCK
 
 	for (int i = 0; i < numThreads; i++){
-		if(pthread_create(&thread_id[i], NULL, threadWork, NULL)){
+
+		if(pthread_create(&thread_id[i], NULL, threadWork, (void*)str)){
 			printf("Thread Creation Failure\n");
 		}
-	assert(pthread_mutex_unlock(&lock)==0); //UNLOCK
-		
 	}
+	assert(pthread_mutex_unlock(&lock)==0); //UNLOCK
+			
 }
 
 
